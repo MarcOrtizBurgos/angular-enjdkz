d01c7b04bd64761d451a391961089ebd657692fa
MarcOrtizBurgos <71668593+MarcOrtizBurgos@users.noreply.github.com>
2023-02-07 19:24:12 +0100

    3 files modified, 9 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
-import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
@@ -19,19 +17,16 @@ import { ShippingComponent } from './shipping/shipping.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'Users/:userId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
-      { path: 'shipping', component: ShippingComponent },
     ]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
-    ShippingComponent,
   ],
   bootstrap: [AppComponent],
 })
