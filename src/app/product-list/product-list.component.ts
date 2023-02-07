e24455515b761e757f6df748dcfeb59d2e632e9c
MarcOrtizBurgos <71668593+MarcOrtizBurgos@users.noreply.github.com>
2023-02-07 19:14:21 +0100

    5 files modified, 1 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
+import UsersJson from '../../assets/users.json';
+
+interface USERS {
+  id: String;
+  name: String;
+  surname: String;
+  email: String;
+}
 
 @Component({
   selector: 'app-product-list',
@@ -8,15 +15,7 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  products = products;
-
-  share() {
-    window.alert('The product has been shared!');
-  }
-
-  onNotify() {
-    window.alert('You will be notified when the product goes on sale');
-  }
+  Users: USERS[] = UsersJson;
 }
 
 /*
